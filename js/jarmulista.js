d7877b34267ebdf815ad4fc756f3a2cf752a1d10
Dancs Dániel <dancsdani1@gmail.com>
2021-12-13 11:55:13 +0100

    asd

--- js/jarmulista.js
+++ js/jarmulista.js
@@ -7,15 +7,15 @@ function tabla(adatok){
     let sz="";
 
     for (let elem of adatok) {
-        sz+='<div class="card" style="width:400px;">';
-            sz+='<img class="card-img-top" src="./kepek/'+elem.autok_kep+'">';
+        sz+='<div class="card" style="width:400px; margin: 20px !important;">';
+            sz+='<img class="card-img-top" src="./kepek/'+elem.autok_kep+'"" style="padding: 10px 10px 10px 10px; border-radius: 25px;">';
                 sz+='<div class="card-body">';
                     sz+='<h6 class="card-title">'+elem.autok_marka+' '+elem.autok_tipus+'</h6>';
                     sz+='<li class="card-text">Gyártás éve: '+elem.autok_gyartaseve+'</li>';
                     sz+='<li class="card-text">Teljesítmény: '+elem.autok_teljesitmeny+'</li>';
                     sz+='<li class="card-text">Férőhelyek: '+elem.autok_ferohely+'</li>';
                     sz+='<li class="card-text">Bérlés napidíja: '+elem.autok_dij+' kredit</li>';
-                    sz+='<a href="#" class="btn btn-success" style="margin-top: 10px;">Autó foglalása</a>';
+                    sz+='<a href="#" class="btn btn-success" style="display: block">Autó foglalása</a>';
                 sz+='</div>';
         sz+='</div>';
     }
