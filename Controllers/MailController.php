cd6bb00ff333a463f823a9d49672499febf99e42
Dancs Dániel <dancsdani1@gmail.com>
2022-01-31 10:32:38 +0100

    email

--- Controllers/MailController.php
+++ Controllers/MailController.php
@@ -1,8 +1,25 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'Includes/PHPMailer/Exception.php';
+require 'Includes/PHPMailer/PHPMailer.php';
+require 'Includes/PHPMailer/SMTP.php';
 
 function reservationMail($email, $text){
-        require "../Includes/mailconfig.inc.php";
-        global $mail;
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'autokolcsonzoinfo@gmail.com';
+        $mail->Password   = 'Autokolcsonzo2022';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+        $mail->CharSet = 'UTF-8';
 
         $mail->setFrom('autokolcsonzoinfo@gmail.com');
         $mail->addAddress($email);
@@ -12,4 +29,6 @@ function reservationMail($email, $text){
         $mail->Body    = $text;
 
         $mail->send();
+    } catch (Exception $e) {
+    }
 }
