a9554a25dbbf9edb92d959d4cc53b2b8e5b76be8
Dancs Dániel <dancsdani1@gmail.com>
2022-01-31 09:56:51 +0100

    mail

--- Controllers/MailController.php
+++ Controllers/MailController.php
@@ -1,25 +1,8 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-require 'Includes/PHPMailer/Exception.php';
-require 'Includes/PHPMailer/PHPMailer.php';
-require 'Includes/PHPMailer/SMTP.php';
-
 function reservationMail($email, $text){
-    $mail = new PHPMailer(true);
-
     try {
-        $mail->SMTPDebug = SMTP::DEBUG_OFF;
-        $mail->isSMTP();
-        $mail->Host       = 'smtp.freemail.hu';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'autokolcsonzoinfo@freemail.hu';
-        $mail->Password   = 'Autokolcsonzo2022';
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port       = 587;
-        $mail->CharSet = 'UTF-8';
+        require "Includes/mailconfig.inc.php";
+        global $mail;
 
         $mail->setFrom('autokolcsonzoinfo@freemail.hu');
         $mail->addAddress($email);
